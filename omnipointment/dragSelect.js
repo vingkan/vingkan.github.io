24b1f3e16e05fa7d85008a7a61f25cb093750cc7
Vinesh Kannan <vingkan@gmail.com>
2015-08-30 21:53:32 -0500

    Released drag select prototype

--- omnipointment/dragSelect.js
+++ omnipointment/dragSelect.js
@@ -1,42 +1,94 @@
 function createSlot(date){
 	var timestamp = date.getTime();
-	var html = '<div class="slot" id="' + timestamp + '" unselectable="on" onmousedown="from(' + timestamp + ')" onmouseup="to(' + timestamp + ')">' + timestamp + '</div>';
+	var html = '<div class="slot" id="' + timestamp + '" onmousedown="from(' + timestamp + ')" onmouseup="to(' + timestamp + ')">' + timestamp + '</div>';
 	return html;
 }
 
 function toggle(timestamp){
 	var slot = document.getElementById(timestamp);
-	if(slot.style.background == '#80ff00'){
-		slot.style.background = '#cccccc';
+	if(slot != null){
+		if(slot.style.background == '#80ff00' || slot.style.background == 'rgb(128, 255, 0)'){
+			slot.style.background = '#cccccc';
+		}
+		else{
+			slot.style.background = '#80ff00';
+		}
 	}
 	else{
-		slot.style.background = '#80ff00';
+		console.log('ERROR: Timestamp: ' + timestamp + ' not included in grid.')
 	}
 }
 
 var fromSlot = null;
 var toSlot = null;
 var minuteInterval = 15;
+var oneSlot = 1000 * 60 * minuteInterval; //1000 milliseconds * 60 seconds * 15 minutes
+/* Apparently the diference between two same times, one day apart from each other is equal to the below, but times 27 hours instead of 24...
+I'll get to the bottom of it eventually, but for now, it's magic number time!
+var oneDay = 1000 * 60 * 60 * 24; //1000 ms * 60 s * 60 minutes * 24 hours */
+var oneDay = 97200000; //Yippeeeee Magic Numbers!
+
+
+function toggleRow(firstDate, days){
+	var targetSlot = null;
+	//console.log("From: " + firstDate + " and go over " + days + " days.")
+	while(days != 0){
+		targetSlot = firstDate + ((days) * oneDay);
+		toggle(targetSlot);
+		if(days > 0){
+			days--;
+		}
+		else if(days < 0){
+			days++;
+		}
+	}
+	toggle(firstDate);
+	if(targetSlot != null){
+		return targetSlot;
+	}
+	else{
+		console.log("toggleRow() says: NULL SLOT RETURNED.");
+	}
+}
 
 function toggleGrid(){
-	var fifteenMinutes = 1000 * 60 * 15; //1000 milliseconds * 60 seconds * 15 minutes
-	var oneDay = 1000 * 60 * 60 * 24; //1000 ms * 60 s * 60 minutes * 24 hours
 	var fromDate = new Date(fromSlot);
 	var toDate = new Date(toSlot);
-	var difference = new Date(toDate.getTime() - fromDate.getTime());
-	console.log("Days Difference: " + difference.getTime()/oneDay);
-	var daysDifference = difference.getTime()/oneDay;
-	var dayColumns = Math.ceil(daysDifference);
-	console.log("DaysX: " + dayColumns)
-	var timestamp = 0;
-	for(var d = 0; d < dayColumns; d++){
-		timestamp = fromDate.getTime() + (d * oneDay);
-		console.log(timestamp)
-		toggle(timestamp);
+
+	var direction = null;
+	var daysDifference = (toSlot - fromSlot) / oneDay;
+	var days = 0;
+	if(daysDifference > 0){
+		if((toSlot - fromSlot) < oneDay){
+			days = Math.ceil(daysDifference);
+			direction = -1;
+		}
+		else{
+			days = Math.floor(daysDifference);
+			direction = 1;
+		}
+	}
+	else if(daysDifference < 0){
+		if((fromSlot - toSlot) > oneDay){
+			days = Math.ceil(daysDifference);
+			direction = -1;
+		}
+		else{
+			days = Math.floor(daysDifference);
+			direction = 1;
+		}
 	}
 
-		
-
+	var firstSlot = fromSlot;
+	var lastSlot = null;
+	var filled = false;
+	while(!filled){
+		lastSlot = toggleRow(firstSlot, days);
+		firstSlot += direction * oneSlot;
+		if(lastSlot == toSlot || firstSlot == toSlot){
+			filled = true;
+		}
+	}
 }
 
 function from(slot){
