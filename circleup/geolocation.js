6164903b6e73faf03fe8fc50026dafe00fa1de0f
Vinesh Kannan <vingkan@gmail.com>
2015-09-13 05:18:35 -0500

    full marker loop

--- circleup/geolocation.js
+++ circleup/geolocation.js
@@ -16,15 +16,25 @@ function initialize(){
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
     var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+
+    var infowindow = new google.maps.InfoWindow();
+
     var marker, i;
+
     for (i = 0; i < locations.length; i++) {  
       marker = new google.maps.Marker({
         position: new google.maps.LatLng(locations[i][1], locations[i][2]),
         map: map,
         title: locations[i][0]
-    });
-    }
+      });
 
+      google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        return function() {
+          infowindow.setContent(locations[i][0]);
+          infowindow.open(map, marker);
+        }
+      })(marker, i));
+    }
 }
 
 function getLocation() {
