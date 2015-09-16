d605ba06e146fe40dda00de3d0488255e5ac71d3
Vinesh Kannan <vingkan@gmail.com>
2015-09-16 16:27:36 -0500

    Test OMS

--- circleup/dev/geolocation.js
+++ circleup/dev/geolocation.js
@@ -100,7 +100,7 @@ function isLetter(char){
 
 function toTitleCase(str)
 {
-    return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+    return str;
 }
 
 function getLocation() {
