0fb5e5d3d57547489c1b0c6b86ce57fff00aa91b
Vinesh Kannan <vingkan@gmail.com>
2015-09-15 20:20:52 -0500

    Test label with one char

--- circleup/dev/geolocation.js
+++ circleup/dev/geolocation.js
@@ -52,6 +52,7 @@ function initialize(){
         position: new google.maps.LatLng(locations[i][1], locations[i][2]),
         map: map,
         title: locations[i][0],
+        label: locations[i][0].charAt(0),
         animation: google.maps.Animation.DROP
       });
 
