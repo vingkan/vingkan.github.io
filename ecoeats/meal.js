8bcfd07a20fcba772222b53665f7eed499004afe
Vinesh Kannan <vingkan@gmail.com>
2015-09-23 19:28:50 -0500

    Test text styles for fluid UX

--- ecoeats/meal.js
+++ ecoeats/meal.js
@@ -1,10 +1,16 @@
 function loadMeals(){
 	var upcomingMeals = document.getElementById('upcomingMeals');
 	if(meals.length > 0){
+		meals.sort(function(a, b){
+			return sortMealsByTimestamp(a, b);
+		});
 		upcomingMeals.innerHTML = "";
 		for(var m = 0; m < meals.length; m++){
 			upcomingMeals.innerHTML += '<p>' + meals[m] + '</p>';
 		}
+	}
+	else{
+		upcomingMeals.innerHTML += '<p>No meals are currently listed. Host your own!</p>';
 	}
 }
 
@@ -22,6 +28,14 @@ Meal.prototype.toString = function(){
 	return this.name + " by " + this.chef + ": " + moment(this.timestamp).calendar() + " for " + this.capacity + " people at " + money(this.price) + " each.";
 }
 
+Meal.prototype.toHTML = function(){
+
+}
+
 function money(value){
 	return "$" + value.toFixed(2);
+}
+
+function sortMealsByTimestamp(meal1, meal2){
+	return meal1.timestamp - meal2.timestamp;
 }
