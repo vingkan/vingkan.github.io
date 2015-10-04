9ae17eede6c56bc10215ce69822df19da753c534
Vinesh Kannan <vingkan@gmail.com>
2015-10-04 03:46:42 -0500

    Upload game with clouds

--- treetalk/game/board.js
+++ treetalk/game/board.js
@@ -3,6 +3,7 @@ Board.prototype.size = 400; //Pixels as Double
 Board.prototype.roadSize = 40; //Pixels as Double
 Board.prototype.roads = [] //Array of Roads
 Board.prototype.trees = [] //Array of Trees
+Board.prototype.clouds = 0 //Integer
 
 function Board(gameSpaceID){
 	this.gameSpace = gameSpaceID || "gameSpace";
@@ -10,9 +11,17 @@ function Board(gameSpaceID){
 	this.roadSize = 40;
 	this.roads = [];
 	this.trees = [];
+	this.clouds = 0;
+}
+
+Board.prototype.pollute = function(){
+	var pollution = 0;
+	pollution = 10 * this.roads.length;
+	this.clouds += pollution;
 }
 
 Board.prototype.update = function(){
+	this.pollute();
 	var gameSpaceDiv = document.getElementById(this.gameSpace);
 	gameSpaceDiv.innerHTML = this.toHTML();
 }
@@ -23,8 +32,14 @@ Board.prototype.toHTML = function(){
 	var xOffSet = this.roadSize;
 	var yOffSet = this.roadSize + (-1 * ((this.size - this.roadSize) / 2));
 	var offsetInterval = this.roadSize * 3;
+	for(var c = 0; c < this.clouds; c++){
+		var xRandom = Math.random() * ((this.size) + (yOffSet / 4));
+		var yRandom = Math.random() * ((this.size - this.roadSize));
+		html += '<div class="cloud" ';
+		html += 'style="margin-left:' + xRandom + 'px;';
+		html += 'margin-top:' + yRandom + 'px;"></div>';
+	}
 	for(var r = 0; r < this.roads.length; r++){
-		console.log('x: ' + xOffSet + ', y: ' + yOffSet);
 		if(this.roads[r].vertical){
 			html += this.roads[r].toHTML(yOffSet.toString() + 'px');
 			yOffSet += offsetInterval;
--- treetalk/game/car.js
+++ treetalk/game/car.js
@@ -1,14 +1,18 @@
 Car.prototype.name = "" //String
 Car.prototype.color = "" //Color as String
+Car.prototype.appearanceOptions = 36; //Body Type as Integer
+Car.prototype.appearance = 1; //Body Type as Integer
 
 function Car(name, color){
 	this.name = name || "Car";
 	this.color = color || "red";
+	this.appearance = Math.round((Math.random() * (this.appearanceOptions - 1)) + 1);
 }
 
 Car.prototype.toHTML = function(){
 	var html = '';
-	html += '<div class="car" style="background:' + this.color + ';">';
-	html += this.name + '</div>';
+	html += '<div class="car" style="background-image:';
+	html += 'url(style/cars/path' + (2192 + this.appearance) * 2 + '.png);">';
+	html += '</div>';
 	return html;
 }
